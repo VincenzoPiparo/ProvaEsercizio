a7c2cfeadb5495115c79da5cd1489d0d7fefcb94
VincenzoPiparo <v.pipar.97@gmai.com>
2022-05-10 17:39:50 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System; //riferimento della libreria di base 
+
+namespace MyApp // Note: actual namespace depends on the project name. 
+{
+    internal class Program
+    {
+        static void Main(string[] args) //entry point 
+        {
+            //namespace, classe, metodo 
+            int v1, v2;
+            Console.WriteLine("Digita primo numero:");
+
+            if (!int.TryParse(Console.ReadLine(), out v1))
+            {
+                Console.WriteLine("Errore, non è stato inserito un numero");
+                System.Environment.Exit(1);
+            }
+
+            Console.WriteLine("Digita secondo numero:");
+
+            if (!int.TryParse(Console.ReadLine(), out v2))
+            {
+                Console.WriteLine("Errore, non è stato inserito un numero");
+                System.Environment.Exit(1);
+            }
+
+            System.Console.WriteLine("Risultato: {0}, {1}",
+                Add(v1, v2), "Ciao");
+        }
+        static int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+    }
+}
